5a0e09cdb564708b8adb110b01cd052430e8b688
james gunn <jamesgunn7@outlook.com>
2018-08-13 13:09:02 +0100

    checkbox can be flipped

--- app/playground/playground.component.tsx
+++ app/playground/playground.component.tsx
@@ -1,6 +1,8 @@
 import { observer } from 'mobx-react';
 import * as React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { Constants } from 'expo';
+import { CheckBox, List, ListItem, Button, } from 'native-base';
 
 const settings = require('../../assets/appbuildersettings.json');
 
@@ -10,9 +12,10 @@ export interface PlaygroundProps {
 
 @observer export class Playground extends React.Component<PlaygroundProps, any> {
 
-
-
     ref;
+    _handleTextChangeRefrence: any;
+    _handleSendButtonPressRefrence: any;
+
 
 
 
@@ -20,10 +23,39 @@ export interface PlaygroundProps {
         super(props);
 
         this.state = {
-            design: settings
-        }
+            design: settings,
+            inputValue: '',
+            toDoListData: [],
+            checkBox: true,
+        };
 
+        this._handleTextChangeRefrence = this._handleTextChange.bind(this);
+        this._handleSendButtonPressRefrence = this._handleSendButtonPress.bind(this);
     }
+
+    _handleTextChange = (value) => {
+        const inputValue = value;
+        this.setState(() => ({
+            inputValue,
+        }))
+    };
+
+    _handleSendButtonPress = () => {
+        if (!this.state.inputValue) {
+            return;
+        }
+        let stuffGetEdit = this.state.toDoListData;
+
+        let newData = {
+            textContent: this.state.inputValue,
+        };
+
+        stuffGetEdit.push(newData);
+        this.setState(() => ({
+            toDoListData: stuffGetEdit,
+            inputValue: '',
+        }));
+    };
 
     componentDidMount() {
 
@@ -35,45 +67,101 @@ export interface PlaygroundProps {
         // Text
         this.setState({
             design: data
+
         });
         //      ComponentService.updateComponent(data.key, data);
     }
 
+
     render() {
-        console.log('RENDERING');
         return (
+            <View style={styles.container}>
+                <View style={styles.formView}>
+                    <TextInput
+                        style={styles.inputForm}
+                        value={this.state.inputValue}
+                        onChangeText={this._handleTextChangeRefrence}
+                        placeholder="Input todo"
+                    />
+                    <Button bordered light
+                        onPress={this._handleSendButtonPressRefrence}
+                    >
+                        <Text>Add</Text>
 
+                    </Button>
+                </View>
 
+                <List>
+                    {this.state.toDoListData.map((item, key) => {
+                        return (<ListItem key={key} >
+                            <Text>{item.textContent}</Text>
+                            <Button
 
-            // < MapViewComponent />
+                                onPress={(toDoListDataItemId) => {
 
-            // <View ref={ref => this.ref = ref} onLayout={async ({ nativeEvent }) => {
-            //     console.log('HEIGHT', nativeEvent.layout.height);
-            //     const result = await takeSnapshotAsync(this.ref, {
-            //         result: 'base64',
-            //         quality: 1,
-            //         format: 'png'
-            //     });
+                                    let toDoListData = this.state.toDoListData;
+                                    toDoListData.splice(toDoListDataItemId, 1);
+                                    this.setState(() => ({
+                                        toDoListData: toDoListData,
+                                    }));
+                                }
+                                }
+                            >
 
-            //     WebsocketService.socket.emit('screenshotUpdate', {
-            //         key: 'test',
-            //         imageUri: result,
-            //         format: 'png',
-            //         imageType: 'base64'
-            //     });
-            // }}>
-            //     <Text>Hi Calum</Text>
-            // </View>
+                                <Text>Delete</Text>
+                            </Button>
+                            <CheckBox checked={this.state.checkBox} onPress={() => {
+                                const newstate = !this.state.checkBox;
+                                this.setState({
+                                    checkBox: newstate
+                                });
 
-            <View style={{ flex: 1 }} collapsable={false}>
-                <Text>calum is a nonce</Text>
-            </View>
+                            }} />
+                        </ListItem>
+                        );
+                    })}
+                </List>
 
-
-            // <VideoComponent
-            //     id='jamesiscool'
-            // />
-
+            </View >
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: 'white',
+    },
+    formView: {
+        borderBottomWidth: 1,
+        borderColor: '#ccc',
+        paddingBottom: 8,
+    },
+    inputForm: {
+        backgroundColor: '#fff',
+        width: 320,
+        height: 40,
+        padding: 8,
+        marginBottom: 8,
+    },
+    todoItem: {
+        alignItems: 'center',
+        padding: 8,
+        width: 320,
+        borderBottomWidth: 1.5,
+        borderColor: '#e0e0e0',
+        backgroundColor: '#fff',
+
+        flex: 1,
+        flexDirection: 'row',
+    },
+    todoText: {
+        flex: 1,
+    },
+});
+
+
+
+
